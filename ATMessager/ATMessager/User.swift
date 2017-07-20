4477ed09d465287b6a08ba57b2792bd2c4004aad
Aashish Tyagi <aashish.tyagi@crmnext.in>
2017-07-20 17:58:42 +0530

    Implement user listing and Some refactoring

---
+++ ATMessager/ATMessager/User.swift
@@ -0,0 +1,33 @@
+//
+//  User.swift
+//  ATMessager
+//
+//  Created by MOBILE MAC1 on 7/20/17.
+//  Copyright © 2017 MOBILE MAC1. All rights reserved.
+//
+
+import UIKit
+
+class User: NSObject {
+
+    var id = ""
+    var name = ""
+    var mobile = ""
+    var email = ""
+    var location = ""
+    
+    override init () {
+        // uncomment this line if your class has been inherited from any other class
+        //super.init()
+    }
+    
+    convenience init(dictionary: Dictionary<String, AnyObject>) {
+        self.init()
+        
+        id = dictionary["id"] as! String
+        email = dictionary["email"] as! String
+        name = dictionary["name"] as! String
+        mobile = dictionary["mobile"] as! String
+        location = dictionary["location"] as! String
+    }
+}
--- ATMessager/ATMessager/UserListViewController.swift
+++ ATMessager/ATMessager/UserListViewController.swift
@@ -8,11 +8,64 @@
 
 import UIKit
 import FirebaseAuth
+import FirebaseDatabase
 
-class UserListViewController: UIViewController {
+class UserListViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+
+    var userList = [User]()
+    @IBOutlet weak var tableView: UITableView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        //A Table Height & Get User listing
+        tableView.rowHeight = 65
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "UserCell")
+        getUserList()
+    }
+    
+    
+    // MARK: Fetch user list 
+    func getUserList(){
+        
+        let dbRef = FIRDatabase.database().reference()
+        let userID = FIRAuth.auth()?.currentUser?.uid
+        dbRef.child("Users").observeSingleEvent(of: .value, with: { (snapshot) in
+            
+            //1] Print Full data values
+            let value = snapshot.value as? NSDictionary
+            for (key, data) in value! {
+                let keyName = key as! String
+                if userID != keyName{
+                    
+                    let user = User.init(dictionary: data as! Dictionary<String, AnyObject>)
+                    self.userList.append(user)
+                }
+            }
+            //2] Reload Table Cell
+            self.tableView.reloadData()
+            
+        }) { (error) in
+            print(error.localizedDescription)
+        }
+    }
+    
+    
+// MARK: UITableView Method
+    
+     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return userList.count
+    }
+    
+     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = UITableViewCell(style: UITableViewCellStyle.subtitle, reuseIdentifier: "UserCell")
+//        var cell = tableView.dequeueReusableCell(withIdentifier: "UserCell", for: indexPath)
+        
+        let user = userList[indexPath.row]
+        cell.textLabel?.text = user.name
+        cell.detailTextLabel?.text = user.location
+        return cell
     }
     
     //1] Logout Firebase.
